aedc19f43575352467d3ab433386f8b4583bab57
Heine Günther <heine_gunther@hotmail.com>
2023-09-22 19:00:33 +0200

    Added grid

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,33 @@
-import { Calculator } from "./components/Calculator/Calculator";
-import { Calendar } from "./components/Calendar/Calendar";
+import { Grid } from "@mui/material";
+import Calculator from "./components/Calculator/Calculator";
+import Calendar from "./components/Calendar/Calendar";
 import NewsFeed from "./components/NewsFeed/NewsFeed";
 import WeatherWidget from "./components/WeatherWidget/WeatherWidget";
-import Navbar from "./components/navbar/Navbar";
 import StravaFeed from "./components/stravaFeed/StravaFeed";
+import Navbar from "./components/navbar/Navbar";
 
 function App() {
   return (
-    <>
-      <Navbar />
-      <WeatherWidget />
-      <NewsFeed />
-      <Calendar />
-      <Calculator />
-      <StravaFeed />
-    </>
+    <Grid container spacing={2}>
+      <Grid item xs={12}>
+        <Navbar />
+      </Grid>
+      <Grid item xs={8}>
+        <WeatherWidget />
+      </Grid>
+      <Grid item xs={4}>
+        <Calendar />
+      </Grid>
+      <Grid item xs={8}>
+        <NewsFeed />
+      </Grid>
+      <Grid item xs={4}>
+        <Calculator />
+      </Grid>
+      <Grid item xs={4}>
+        <StravaFeed />
+      </Grid>
+    </Grid>
   );
 }
 

c9efad48cf50ae238a48acd97889ae7b0c2b5cd5
Heine Günther <heine_gunther@hotmail.com>
2023-09-23 20:44:03 +0200

    added darkmode

--- src/main.jsx
+++ src/main.jsx
@@ -3,16 +3,11 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
-// import { ThemeProvider, createTheme } from "@mui/material";
-
-// const theme = createTheme;
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    {/* <ThemeProvider theme={theme}> */}
     <BrowserRouter>
       <App />
     </BrowserRouter>
-    {/* </ThemeProvider> */}
   </React.StrictMode>
 );
